5893da12777afe77301a56e0b044bf6eb3603393
Samir Sayyad <samir.saiad@gmail.com>
2021-07-07 11:53:11 +0430

    managing notifications

--- static/js/handleRocketChatNotifications/methods/unreadChangedBySubscription.js
+++ static/js/handleRocketChatNotifications/methods/unreadChangedBySubscription.js
@@ -2,7 +2,7 @@ exports.unreadChangedBySubscription = function unreadChangedBySubscription (data
     if(data.alert == true){
         const padId = clientVars.padId;
         const userId = pad.getUserId();
-
+        
         if(data.unread == 0){
             var unreadNotificationTemplate = $('#ep_rocketchat_unreadNotification').tmpl(data);
             $(`#${data.name}_notification`).html(unreadNotificationTemplate);
@@ -11,7 +11,34 @@ exports.unreadChangedBySubscription = function unreadChangedBySubscription (data
             var mentionNotificationTemplate = $('#ep_rocketchat_mentionNotification').tmpl(data);
             $(`#${data.name}_notification`).html(mentionNotificationTemplate);
         }
+
+        var rowContainer=$(`#${data.name}_container`) ;
+        if(rowContainer.length){
+            var elementStatus = checkInView(rowContainer, true );
+            console.log(elementStatus,"ss")
+            if (elementStatus.topLocation == false){
+                $("#bottomNewMention").css({"display":"block"})
+            }
+        }
+        
+
     }else{
         $(`#${data.name}_notification`).empty()
     }
+    
 }
+
+function checkInView(elem,partial){
+    var container = $("#toc");
+    var contHeight = container.height();
+    var contTop = container.scrollTop();
+    var contBottom = contTop + contHeight ;
+
+    var elemTop = $(elem).offset().top - container.offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    var isTotal = (elemTop >= 0 && elemBottom <=contHeight);
+    var isPart = ((elemTop < 0 && elemBottom > 0 ) || (elemTop > 0 && elemTop <= container.height())) && partial ;
+
+    
+    return  { visible : isTotal  || isPart , topLocation:(elemTop < 0 && elemBottom < 0) } ;
+  }
