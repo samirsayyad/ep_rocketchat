14c03a8fa9c4c35b8a66a1bc7229d3b4b6319b39
Samir Sayyad <samir.saiad@gmail.com>
2021-09-30 11:16:40 +0330

    rocketchat notification to ep_table_of_contents

--- static/js/handleRocketChatNotifications/handleRocketChatNotifications.js
+++ static/js/handleRocketChatNotifications/handleRocketChatNotifications.js
@@ -5,9 +5,10 @@ exports.handleRocketChatNotifications = function handleRocketChatNotifications (
     window.addEventListener('message', function(e) {
         const eventName = e.data.eventName;
         const data = e.data.data;
-        // if(eventName == "notification"){
-        //     notificationsMethod(data)
-        // }
+        if(eventName == "notification"){
+            notificationsMethod(data)
+        }
+        console.log(eventName,data )
         if(eventName=="unread-changed-by-subscription"){
             unreadChangedBySubscription(data)
         }
--- static/js/handleRocketChatNotifications/methods/notificationsMethod.js
+++ static/js/handleRocketChatNotifications/methods/notificationsMethod.js
@@ -1,6 +1,7 @@
 exports.notificationsMethod = function notificationsMethod (data){
     if(!data.fromOpenedRoom){ // must be false in order to notify user
-        const headerId = data.notification.payload.name ; 
+        const padId = pad.getPadId();
+        const headerId = (data.notification.payload.name == `${padId}-general-channel`) ? "general" : data.notification.payload.name  ; 
         var unreadNotificationTemplate = $('#ep_rocketchat_unreadNotification').tmpl(data);
             $(`#${headerId}_notification`).html(unreadNotificationTemplate);
     }
