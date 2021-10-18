8381497039b7af398703d64acd57e666876ff7b5
Samir Sayyad <samir.saiad@gmail.com>
2021-10-18 11:50:38 +0330

    fix general highlight badge

--- static/js/handleClientMessageMethod/updateRocketChatIframe.js
+++ static/js/handleClientMessageMethod/updateRocketChatIframe.js
@@ -2,7 +2,10 @@ exports.updateRocketChatIframe = function updateRocketChatIframe(payLoad){
     try{
         $("#ep_rocketchat_iframe").attr({"src": `${payLoad.data.rocketChatBaseUrl}/channel/${payLoad.data.room}?layout=embedded`})
         let room =payLoad.data.room;
-        room = (room == "GENERAL") ? "general" : room;
+        let padId =payLoad.data.room;
+
+        room = (room == `${padId}-general-channel` ) ? "general" : room;
+        console.log(room,"ropo")
         localStorage.setItem(`${room}_unreadCount`,0);
         $(`#${room}_notification`).empty()
 
