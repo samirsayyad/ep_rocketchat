4db792e0c3d6f7c02f3dc7a6e8e60638d4c5f607
Samir Sayyad <samir.saiad@gmail.com>
2021-12-06 11:45:34 +0330

    move css to ep_table_of_contents

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const eejs = require('ep_etherpad-lite/node/eejs/');
 
-exports.eejsBlock_styles = (hookName, args, cb) => {
-    args.content +=
-    "<link href='../static/plugins/ep_rocketchat/static/css/rocketchat.css' type='text/css' rel='stylesheet'>";
-    return cb();
-  };
 
 exports.eejsBlock_scripts = (hook_name, args, cb) => {
   args.content += eejs.require('ep_rocketchat/templates/mentionNotification.html', {}, module);
