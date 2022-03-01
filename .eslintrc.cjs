fb2875f8560faeb0ec1fdcef649a1bb940de45fc
hossein <marzban98@gmail.com>
2022-03-01 12:55:57 +0330

    fix: reconfig eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+'use strict';
+
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('eslint-config-etherpad/patch/modern-module-resolution');
+
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jquery: true,
+    mocha: true,
+  },
+  root: true,
+  extends: 'etherpad',
+};
