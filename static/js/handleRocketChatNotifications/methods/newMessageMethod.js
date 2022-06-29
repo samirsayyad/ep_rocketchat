fb4689e9f1ea0d19be4a236d800b7758b21d3b8f
Hossein Marzban <marzban98@gmail.com>
2022-06-29 09:55:59 +0430

    fix: sync toc and header counter

--- static/js/handleRocketChatNotifications/methods/newMessageMethod.js
+++ static/js/handleRocketChatNotifications/methods/newMessageMethod.js
@@ -5,11 +5,14 @@ const removeNewMentionHelper = require('./helper/newMentionHelper').removeNewMen
 const notificationHelper = require('./helper/notificationHelper');
 const pushMethod = require('./pushMethod').pushMethod;
 
+const $bodyAceOuter = () => $(document).find('iframe[name="ace_outer"]').contents();
+
 exports.newMessageMethod = (data) => {
   const padId = pad.getPadId();
   const roomId = data.name;
   const userId = pad.getUserId();
   const headerId = (roomId === `${padId}-general-channel`) ? 'general' : roomId;
+  const isMobile = clientVars.userAgent.isMobile;
 
   const lastActiveHeader = notificationHelper.getLastActiveHeader() || '';
   if (lastActiveHeader.toLowerCase() === headerId) return;
@@ -43,5 +46,39 @@ exports.newMessageMethod = (data) => {
     unreadNotificationTemplate = $('#ep_rocketchat_unreadNotification').tmpl({unread: unReadCount});
     notificationElement.html(unreadNotificationTemplate);
     removeNewMentionHelper(realHeaderId);
+
+    let unreadCount = unReadCount;
+    let $el = $bodyAceOuter().find('iframe')
+        .contents()
+        .find('#innerdocbody')
+        .find(`[headerid="${realHeaderId}"]`)[0];
+
+    if ($el) {
+      $el = $el.shadowRoot;
+      // change the inline icon when notification counter is 0
+      if (!isMobile) {
+        if (unreadCount === 0) {
+          $el.querySelector('.bubbleNotify').style.display = 'none';
+          $el.querySelector('.mobileIcon').style.display = 'block';
+          $el.querySelector('.mobileIcon').style.marginTop = '5px';
+        } else if (unreadCount > 0) {
+          $el.querySelector('.bubbleNotify').style.display = 'block';
+          $el.querySelector('.mobileIcon').style.display = 'none';
+          $el.querySelector('.mobileIcon').style.marginTop = '0';
+        }
+      }
+
+      if (unreadCount > 9) {
+        unreadCount = '9+';
+        if (isMobile) {
+          $el.querySelectorAll('.counter').forEach((el) => {
+            el.style.marginLeft = '-7px';
+          });
+        }
+      }
+      $el.querySelectorAll('.counter').forEach((el) => {
+        el.innerText = unreadCount;
+      });
+    }
   }
 };
